9b399798d6926d3fd853bb6c083ac756f67ca8c3
Muhammad Amin <muhdmamin@gmail.com>
2022-09-11 13:53:51 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-int main()
+int void(main)
 
-{
-
-	    puts("Programming is like building a multilingual puzzle");
-
-		        return 0;
+{	
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 
 }
