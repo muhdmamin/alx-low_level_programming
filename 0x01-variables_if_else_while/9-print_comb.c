306dfc01eb2065c4ee2426020ac2abcd137babea
Muhammad Amin <muhdmamin@gmail.com>
2022-09-12 14:45:38 -0700

    s

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num != 9)
+		if (num != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	return(0);
+	return (0);
 }
